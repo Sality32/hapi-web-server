c562f576490d4128132ec966ed0346c1553dc9ae
Yoga Subagja <42853517+Sality32@users.noreply.github.com>
2024-08-29 21:17:24 +0700

    fix bug host value

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost'
+    host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
   });
 
   server.route({
